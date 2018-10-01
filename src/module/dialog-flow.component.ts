1cdc8fb963b484a1a8e9c38c6eb998ebf30f5cf9
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-10-01 16:50:07 +0100

    Added basics for provider approach

--- src/module/dialog-flow.component.ts
+++ src/module/dialog-flow.component.ts
@@ -1,10 +1,22 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable, Provider } from '@nestjs/common';
 import { DialogFlowFulfillmentResponse } from '../interfaces/dialog-flow-fulfillment-response.interface';
 import { DialogFlowResponse } from '../interfaces/dialog-flow-response.interface';
 
+interface MethodProvider {
+	provider: string;
+	method: string;
+}
+
+interface MethodsTree {
+	[name: string]: MethodProvider[];
+}
+
 @Injectable()
 export class DialogFlowService {
-	constructor(@Inject('Handlers') private readonly handlers: Map<string, any>) {}
+	private readonly providers: { [s: string]: Provider }[];
+	private readonly methods: MethodsTree;
+
+	constructor() {}
 
 	public async handleIntentOrAction(
 		dialogFlowResponse: DialogFlowResponse,
@@ -12,8 +24,17 @@ export class DialogFlowService {
 		const intent = dialogFlowResponse.queryResult.intent.displayName;
 		const action = dialogFlowResponse.queryResult.action;
 
-		const handler = this.handlers.get(intent) || this.handlers.get(action);
-		if (!handler) {
+		const handlers = [];
+
+		if (this.methods.hasOwnProperty(intent)) {
+			handlers.push(this.methods[intent]);
+		}
+
+		if (this.methods.hasOwnProperty(action)) {
+			handlers.push(this.methods[action]);
+		}
+
+		if (!handlers) {
 			throw new Error(
 				`Unknown handler for ${
 					intent
@@ -25,7 +46,23 @@ export class DialogFlowService {
 			);
 		}
 
+		//TODO verify if I need to call multiple handlers or just one singular handler
 		const fulfillment = await handler.call(this, dialogFlowResponse);
 		return fulfillment as DialogFlowFulfillmentResponse;
+	}
+
+	protected addProvider(provider: Provider) {
+		this.providers[provider.constructor.name] = provider;
+	}
+
+	public addMethod(name: string, provider: Provider, method: string) {
+		this.addProvider(provider);
+		this.methods[name] = [
+			...this.methods[name],
+			{
+				provider: provider.constructor.name,
+				method,
+			},
+		];
 	}
 }
--- src/module/dialog-flow.module.ts
+++ src/module/dialog-flow.module.ts
@@ -1,14 +1,24 @@
 import { DialogFlowAuthorizationMiddleware } from '../middlewares/dialog-flow-authorization.middleware';
 import { DialogFlowController } from './dialog-flow.controller';
 import { DialogFlowService } from './dialog-flow.component';
-import { DynamicModule, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { provider } from './dialog-flow.provider';
+import {
+	DynamicModule,
+	MiddlewareConsumer,
+	Module,
+	NestModule,
+	OnModuleInit,
+} from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
 import { WebHookConfig } from '../interfaces/web-hook-config.interface';
+import { ModulesContainer } from '@nestjs/core/injector';
+import { MetadataScanner } from '@nestjs/core/metadata-scanner';
+import { DIALOG_FLOW_ACTION, DIALOG_FLOW_INTENT, nestMetadata } from '../constant';
+import 'reflect-metadata';
 
 @Module({
-	providers: [DialogFlowService, provider],
+	providers: [DialogFlowService],
 })
-export class DialogFlowModule implements NestModule {
+export class DialogFlowModule implements NestModule, OnModuleInit {
 	public static forRoot(webHookConfig?: WebHookConfig): DynamicModule {
 		webHookConfig = {
 			basePath: 'web-hooks',
@@ -20,6 +30,49 @@ export class DialogFlowModule implements NestModule {
 			module: DialogFlowModule,
 			controllers: [DialogFlowController.forRoot(webHookConfig)],
 		};
+	}
+
+	constructor(
+		private readonly moduleRef: ModuleRef,
+		private readonly modulesContainer: ModulesContainer,
+		private readonly dialogFlowService: DialogFlowService,
+	) {}
+
+	public onModuleInit(): any {
+		//TODO loop through modules container and find DIALOG_FLOW_HANDLER in reflect for handlers
+		const metadataScanner = new MetadataScanner();
+		const modules = [...this.modulesContainer.values()];
+
+		modules.forEach(({ metatype }) => {
+			const metadata = Reflect.getMetadata(nestMetadata.COMPONENTS, metatype) || [];
+			const components = [...metadata.filter(metatype => typeof metatype === 'function')];
+
+			components.map(component => {
+				const reflectedMetadata = metadataScanner
+					.scanFromPrototype(null, component.prototype, method => {
+						const intentOrAction =
+							Reflect.getMetadata(
+								DIALOG_FLOW_INTENT,
+								Reflect.getOwnPropertyDescriptor(component.prototype, method).value,
+							) ||
+							Reflect.getMetadata(
+								DIALOG_FLOW_ACTION,
+								Reflect.getOwnPropertyDescriptor(component.prototype, method).value,
+							);
+
+						return intentOrAction ? { handler: component, intentOrAction, method } : null;
+					})
+					.filter(v => v);
+
+				[...reflectedMetadata].forEach(metadata => {
+					this.dialogFlowService.addMethod(
+						metadata.intentOrAction,
+						this.moduleRef.get(metadata.handler),
+						metadata.method,
+					);
+				});
+			});
+		});
 	}
 
 	public configure(consumer: MiddlewareConsumer) {
