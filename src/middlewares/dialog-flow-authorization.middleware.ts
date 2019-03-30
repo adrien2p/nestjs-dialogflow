2a1eb3015edbe0127d64bea3cbb9f1a1ab5240a9
Adrien de Peretti <adrien.deperetti.freelance@gmail.com>
2019-03-30 13:13:03 +0100

    Format middleware file

--- src/middlewares/dialog-flow-authorization.middleware.ts
+++ src/middlewares/dialog-flow-authorization.middleware.ts
@@ -3,12 +3,12 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class DialogFlowAuthorizationMiddleware implements NestMiddleware {
 	use(req, res, next) {
-        if (!req.headers.authorization) return next('Missing authorization header');
+		if (!req.headers.authorization) return next('Missing authorization header');
 
-        if (process.env.DIALOG_FLOW_AUTHORIZATION_TOKEN === req.headers.authorization) {
-            return next();
-        } else {
-            return next('Unrecognized authorization token');
-        }
-    }
+		if (process.env.DIALOG_FLOW_AUTHORIZATION_TOKEN === req.headers.authorization) {
+			return next();
+		} else {
+			return next('Unrecognized authorization token');
+		}
+	}
 }
